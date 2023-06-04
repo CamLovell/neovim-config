6b276a6cc3146a6e9e8ca1d0eef0bcf73e642d2a
Cameron Lovell <cameron.e.lovell@gmail.com>
2023-06-04 21:29:42 +1000

    change to kickstart

---
+++ lua/kickstart/plugins/theme.lua
@@ -0,0 +1,13 @@
+return {
+  "sainnhe/gruvbox-material",
+  lazy = false,
+  priority = 1000,
+  opts = {},
+  config = function()
+    vim.g.gruvbox_material_background = 'medium'
+    vim.g.gruvbox_material_transparent_background = 0
+    vim.g.gruvbox_material_foreground = 'material'
+    vim.cmd.colorscheme 'gruvbox-material'
+  end,
+}
+
