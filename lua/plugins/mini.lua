f5b14924b4d1eacd6be785337a6b9234a8c26bbf
Cameron Lovell <cameron.e.lovell@gmail.com>
2025-02-06 19:07:26 +1100

    added mini.files, understood lots

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,8 +1,3 @@
-local function func()
-  local test
-  return test
-end
-
 return {
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
@@ -10,8 +5,8 @@ return {
       -- Better Around/Inside textobjects
       --
       -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
       --  - yinq - [Y]ank [I]nside [N]ext [']quote
+      --  - va)  - [V]isually select [A]round [)]paren
       --  - ci'  - [C]hange [I]nside [']quote
       require('mini.ai').setup { n_lines = 500 }
 
@@ -22,6 +17,10 @@ return {
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
       require('mini.move').setup()
+
+      -- Simple file explorer (with toggle mapped to <leader>fd
+      require('mini.files').setup()
+      vim.keymap.set('n', '<leader>fd', ':lua if not MiniFiles.close() then MiniFiles.open(...) end<CR>', { desc = 'Open [F]ile [D]irectory' })
 
       -- simple indent line only showing current scope
       require('mini.indentscope').setup { draw = { delay = 20, animation = require('mini.indentscope').gen_animation.none() } }
