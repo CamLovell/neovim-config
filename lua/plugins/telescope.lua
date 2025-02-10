b364ffa2b3af2ab423b00872571316fa3632e177
Cameron Lovell <cameron.e.lovell@gmail.com>
2025-02-10 16:39:00 +1100

    added '[S]earch [C]onfig' shortcut

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -49,9 +49,13 @@ return {
       pcall(require('telescope').load_extension, 'ui-select')
 
       -- See `:help telescope.builtin`
+
       vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
       vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Find [G]it [F]iles' })
       vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sc', function()
+        require('telescope.builtin').find_files { cwd = vim.fn.stdpath 'config' }
+      end, { desc = '[S]earch [C]onfig' })
       vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
