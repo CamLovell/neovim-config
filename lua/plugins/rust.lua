604e0d3a2f9d921cd87a3326d8e94580d68f1770
Cameron Lovell <cameron.e.lovell@gmail.com>
2025-02-16 10:44:11 +1100

    added rust specific keybinds

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -3,11 +3,31 @@ return {
     'mrcjkb/rustaceanvim',
     version = '^5', -- Recommended
     lazy = false, -- This plugin is already lazy
-    ['rust-analyzer'] = {
-      cargo = {
-        features = { 'all' },
-      },
-    },
+    -- enabled = false,
+    config = function()
+      vim.g.rustaceanvim = {
+        tools = {},
+        server = {
+          on_attach = function(client, bufnr)
+            vim.keymap.set('n', '<leader>ca', function()
+              vim.cmd.RustLsp 'codeAction'
+            end, { silent = true, buffer = bufnr })
+            vim.keymap.set('n', '<leader>rd', function()
+              vim.cmd.RustLsp 'openDocs'
+            end, { silent = true, buffer = bufnr })
+          end,
+          default_settings = {
+            ['rust-analyzer'] = {
+              -- cargo = {
+              --   features = 'all',
+              -- },
+            },
+          },
+        },
+        -- DAP configuration
+        dap = {},
+      }
+    end,
   },
   {
     'saecki/crates.nvim',
