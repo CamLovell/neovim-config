0b0f7465495a45d4720f791f1863b0227dceb1cb
Cameron Lovell <cameron.e.lovell@gmail.com>
2025-10-23 15:50:33 +1100

    tweaks for ubuntu setup

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -2,7 +2,7 @@ return {
   {
     'linux-cultist/venv-selector.nvim',
     dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
-    branch = 'regexp',
+    -- branch = 'regexp',
     ft = { 'python' },
     opts = {
       -- Your options go here
